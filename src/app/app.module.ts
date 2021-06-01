cc902a37c753221855fe4c79166af0c4a8b86f09
Alberto <murillojaenesalberto@gmail.com>
2021-06-01 19:17:56 +0200

    Añado internalizacion en tiempo de ejecucion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 import { MatSliderModule } from '@angular/material/slider';
+
+//ngx-translate
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { InternationalizationModule } from './internationalization/internationalization.module';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService } from './in-memory-data.service';
-
 import { AppRoutingModule } from './app-routing.module';
-
 import {MatCardModule} from '@angular/material/card';
 import {MatButtonModule} from '@angular/material/button';
 import {MatChipsModule} from '@angular/material/chips';
@@ -29,28 +32,43 @@ import { WizardComponent } from './wizard/wizard.component';
 import { DecisionTreeFormComponent } from './wizard/decision-tree-form/decision-tree-form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// required for AOT compilation
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, '../assets/i18n/', '.json');
+}
+
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
     MatSliderModule,
     AppRoutingModule,
-    HttpClientModule,
     MatCardModule,
     MatButtonModule,
     MatChipsModule,
     MatDialogModule,
     MatDividerModule,
+    // ngx-translate and the loader module
+    HttpClientModule,
+    InternationalizationModule.forRoot({ locale_id: 'en-US' }), // iniating with default language: en-US
+    TranslateModule.forRoot({
 
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
     // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
     // and returns simulated server responses.
     // Remove it when a real server is ready to receive requests.
     HttpClientInMemoryWebApiModule.forRoot(
-      InMemoryDataService, { dataEncapsulation: false }
+      InMemoryDataService, { dataEncapsulation: false, passThruUnknownUrl: true }
     ),
 
     BrowserAnimationsModule
   ],
+  providers: [],
   declarations: [
     AppComponent,
     DashboardComponent,
